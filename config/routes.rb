f883ca4d3c7fb262f5bd1681c98da1b86201f68c
Cobbeec <cobbeec@gmail.com>
2021-02-02 17:31:32 -0600

    added gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :authors
   resources :genres
   resources :users
   resources :books
-  resources :authors
   resources :reviews
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
