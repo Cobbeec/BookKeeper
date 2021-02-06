c6bd0328bfb150854d9b177677fda535192405eb
Cobbeec <cobbeec@gmail.com>
2021-02-06 14:01:13 -0600

    added helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,14 @@ def destroy
     redirect_to root_path 
 end 
 
-
+def create 
+    user = User.find_by_id(params[:id])
+    if user && user.authenticate(params[:user][:password])
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+    else  
+        redirect_to "/login"
+end 
+end 
 
 end 
