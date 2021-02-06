b32a548c1690e0c53435efc72f137ef45294d650
Cobbeec <cobbeec@gmail.com>
2021-02-06 14:53:43 -0600

    added a new form for reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_231932) do
+ActiveRecord::Schema.define(version: 2021_02_06_204843) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_02_02_231932) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
     t.index ["book_id"], name: "index_reviews_on_book_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
