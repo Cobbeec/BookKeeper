ac67d76ed429455de4a4283808cdd8b0a3946c47
Cobbeec <cobbeec@gmail.com>
2021-02-06 15:00:18 -0600

    added review create method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,4 +5,18 @@ class ReviewsController < ApplicationController
     def new 
         @review = Review.new 
     end 
+
+    def create 
+        @review = Review.new(review_params)
+        if @review.save 
+            redirect_to @review 
+        else 
+            render :new 
+        end 
+    end 
+
+    private 
+    def review_params 
+        params.require(:review).permit(:title,:content)
+    end 
 end
