ed405ff5b85c6af9e8d8acfb60b0f863a77b1cba
Cobbeec <cobbeec@gmail.com>
2021-02-09 12:16:40 -0600

    built authorscontroller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,35 @@
 class AuthorsController < ApplicationController
+    def index 
+        @authors = Author.all 
+    end 
+
+    def new 
+        @author = Author.new 
+    end 
+
+    def create
+        @author = Author.create(author_params)
+        if @author 
+          redirect_to author_path(@author)
+        else
+          render :new
+        end
+      end
+ 
+
+    def show 
+        @author = Author.find_by_id(params[:id])
+    end 
+
+    def edit 
+        @author = Author.find_by_id(params[:id])
+    end 
+
+    def update 
+    end 
+
+    private 
+    def author_params
+        params.require(:author).permit(:name)
+      end
 end
