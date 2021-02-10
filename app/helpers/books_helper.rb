e04264d5bca6914f92e3d8df06f445a36e1441e8
Cobbeec <cobbeec@gmail.com>
2021-02-10 17:24:49 -0600

    adding a collection select for author

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,7 +1,7 @@
 module BooksHelper
-    def display_header 
+    def display_header(author) #if we set a brand in our controller
         if @author
-            tag.h2("All the #{author.name} Books")
+            tag.h2("All the #{@author.name} Books")
         else 
             tag.h1("All the books")
     end
