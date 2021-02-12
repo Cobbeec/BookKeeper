f60de39be5f06fa4d4aecf4f71ce633016890f6f
Cobbeec <cobbeec@gmail.com>
2021-02-12 17:04:56 -0600

    cleaning up login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,6 @@ class SessionsController < ApplicationController
         session[:user_id] = user.id
         redirect_to user_path(user)
     else  
-        flash[:message] = user.errors.full_messages.join(",")
         render 'new' 
     end 
     end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id 
-            redirect_to user_path(@user)
+            redirect_to @user 
         else 
             render :new
     end 
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
     def show 
         @user = User.find_by_id(params[:id])
-        redirect_to '/' if !@user
+        redirect_to 'signup' if !@user
     end 
 
     private 
