63939f9e40b4ecc1b13789a6a91f34d32c96cf48
Cobbeec <cobbeec@gmail.com>
2021-02-13 16:42:28 -0600

    added a delete function to review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -45,6 +45,12 @@ class ReviewsController < ApplicationController
        end
      end
 
+     def destroy
+      @review = Review.find_by_id(params[:id])
+      @review.destroy 
+      redirect_to reviews_path
+    end
+
 
     private
     def review_params 
