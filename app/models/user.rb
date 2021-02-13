67c3fa29fdc3bfc4f394702049050f434cbd6049
Cobbeec <cobbeec@gmail.com>
2021-02-13 15:58:44 -0600

    trying to add edit function for review

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :reviews 
     has_many :books 
+    has_many :authors, through: :books 
     validates :username, :email, presence: true
     validates :username, uniqueness: true 
   
